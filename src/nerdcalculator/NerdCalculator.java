9b12fe3e49de373e200f3d168e8ed02f3f04b818
Sebastian Berrio <sberrio09@gmail.com>
2022-09-20 21:27:25 -0500

    Starting repository...

---
+++ src/nerdcalculator/NerdCalculator.java
@@ -0,0 +1,11 @@
+package nerdcalculator;
+
+public class NerdCalculator {
+
+    public static void main(String[] args) {
+        
+        WindowCalculator nerd1 = new WindowCalculator();
+        nerd1.setVisible(true);
+    }
+    
+}
